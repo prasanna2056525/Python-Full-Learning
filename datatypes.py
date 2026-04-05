44b2151cc5bc0246355ad75dcf855a581eb75ee7
Prasanna Gautam <prasannagautam10@gmail.com>
2026-04-05 19:21:53 +0100

    Python Basics

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+
+name  = "prasa" # This is a string variable
+print(name)
+
+age = 25 # This is an integer variable
+print(age)
+
+height = 5.8 # This is a float variable
+print(height)
+
+is_student = True # This is a boolean variable
+print(is_student)
+
---
+++ inputfunction.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Your name is:", name)
+print("Your age is:", age)
+
